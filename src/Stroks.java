2bf4268fc896a9af796cff00528d8c990c680bf4
Dima Arkhipkin <dimka895@gmail.com>
2024-04-02 19:12:14 +0400

    three

--- src/Stroks.java
+++ src/Stroks.java
@@ -2,9 +2,9 @@ public class Stroks {
     public static void main(String[] args){
         String a ="Hello";
         String b ="and";
-        String c ="Bye";
+        String c ="Bye!";
         System.out.println(a+" "+b+" "+c);
         System.out.println();
-        System.out.println("Good "+"morning "+"friends");
+        System.out.println("Good "+"morning "+"friends!");
     }
 }
