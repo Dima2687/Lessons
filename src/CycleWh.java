d03bc618ded62833b5ca7782c58a508bbf41d1f6
Dima Arkhipkin <dimka895@gmail.com>
2024-04-02 13:39:53 +0400

    four

---
+++ src/CycleWh.java
@@ -0,0 +1,9 @@
+public class CycleWh {
+    public static void main(String[] args){
+        int x=0;
+        while (x<5){
+            System.out.println("Heloo "+x);
+            x=x+1;
+        }
+    }
+}
