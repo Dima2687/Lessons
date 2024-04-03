b12d676fad090f0252389b3a1bad591a02f5572d
Dima Arkhipkin <dimka895@gmail.com>
2024-04-03 09:58:22 +0400

    twelve

---
+++ src/MassStrfaech.java
@@ -0,0 +1,12 @@
+public class MassStrfaech {
+    public static void main(String[] args){
+        String[] abc = new String[4];
+        abc[0] = "Доброе";
+        abc[1] = "утро";
+        abc[2] = "мой";
+        abc[3] = "друг";
+        for(String string:abc){
+            System.out.println(string);
+        }
+    }
+}
